b2d3fbc86802a107658391f91e18b0ef7259b551
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-10-02 17:06:09 -0400

    added comment

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -14,6 +14,7 @@ function LoginForm() {
   function handleSubmit(e) {
     e.preventDefault();
     if (!email || !password) return;
+    // addidng comment
 
     login(
       { email, password },
